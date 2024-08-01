daaa9e335c029b10790fd98cecbff66e70f93daa
amar2998 <160626597+amar2998@users.noreply.github.com>
2024-08-01 19:41:44 +0000

    commit

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        long data=5000;
+        int count=(int)data;
+        //float
+        float data2=2.34f;
+        double data3=3.4567654;
+        char name='a';
+        System.out.println(data);
+        System.out.println(data2);
+        System.out.println(data3);
+        System.out.println(count);
+        
+        //implesite conversion which can be converted just by assigning to it
+        // explisite conversion in which we can convert it by (datatype)variable_to_reAssign
+
+    }
+}
